ddea7c24fbc08c5935331dc726bbcf1b26be8d8b
lovely-k15 <kry.lovely@gmail.com>
2024-04-30 04:45:32 +0530

    adding another program in branch1

--- file1.c
+++ file1.c
@@ -1,14 +1,7 @@
+//This is our demo program
 #include <stdio.h>
-int main() {    
-
-    int number1, number2, sum;
-    
-    printf("Enter two integers: ");
-    scanf("%d %d", &number1, &number2);
-
-    // calculate the sum
-    sum = number1 + number2;      
-    
-    printf("%d + %d = %d", number1, number2, sum);
-    return 0;
+int main() {
+   // printf() displays the string inside quotation
+   printf("Hello, World!");
+   return 0;
 }
